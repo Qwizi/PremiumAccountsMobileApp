5e85e1b83e5e4298737afe217d5b143259660e91
Adrian Ciołek <qwizi95@protonmail.com>
2021-01-07 12:56:18 +0100

    PA-86 added main bottom tabs

---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomNavigation, BottomNavigationTab} from '@ui-kitten/components';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export default function MainScreen(props: any) {
+
+    // @ts-ignore
+    const BottomTabBar = ({ navigation, state }) => (
+        <BottomNavigation
+            selectedIndex={state.index}
+            onSelect={index => navigation.navigate(state.routeNames[index])}>
+            <BottomNavigationTab title='Wyszukiwanie'/>
+            <BottomNavigationTab title='Ulubione'/>
+            <BottomNavigationTab title='Moje konto'/>
+        </BottomNavigation>
+    );
+
+    const TabNavigator = () => (
+        <Navigator tabBar={props => <BottomTabBar {...props} />}>
+            <Screen name='Wyszukiwanie' component={() => <div>1</div>}/>
+            <Screen name='Ulubione' component={() => <div>2</div>}/>
+            <Screen name='Moje konto' component={() => <div>3</div>}/>
+        </Navigator>
+    );
+
+    return (
+        <NavigationContainer>
+            <TabNavigator />
+        </NavigationContainer>
+    )
+}
